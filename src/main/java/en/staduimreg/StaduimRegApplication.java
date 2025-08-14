98a49f27e6836c5f28ba0bc96923764b2cbff93c
Eyad Laza <eyadlaza@gmail.com>
2025-08-14 16:37:17 +0200

    Initial commit

---
+++ src/main/java/en/staduimreg/StaduimRegApplication.java
@@ -0,0 +1,13 @@
+package en.staduimreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaduimRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaduimRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/en/staduimreg/StaduimRegApplicationTests.java
@@ -0,0 +1,13 @@
+package en.staduimreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaduimRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

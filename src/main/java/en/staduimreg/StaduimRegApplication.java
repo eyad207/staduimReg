ed96e54c03b4fbd433382ccc4541ff4547719f66
Eyad Laza <eyadlaza@gmail.com>
2025-08-14 16:39:36 +0200

    GG first COMMIT

--- src/main/java/en/staduimreg/StaduimRegApplication.java
+++ src/main/java/en/staduimreg/StaduimRegApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StaduimRegApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StaduimRegApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StaduimRegApplication.class, args);
+    }
 
 }
